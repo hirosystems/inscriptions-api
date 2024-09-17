ced2cd073eb8acbeb325f5ddaee6880f05eaf22f
Rafael Cárdenas <rafael@rafaelcr.com>
2024-09-17 12:37:58 -0600

    feat: add profiler server (#376)
    * feat: add profiler server
    
    * ci: docker compose

--- src/env.ts
+++ src/env.ts
@@ -26,6 +26,8 @@ const schema = Type.Object({
   EVENT_SERVER_BODY_LIMIT: Type.Integer({ default: 20971520 }),
   /** Hostname that will be reported to the ordhook node so it can call us back with events */
   EXTERNAL_HOSTNAME: Type.String({ default: '127.0.0.1' }),
+  /** Port in which to serve the profiler */
+  PROFILER_PORT: Type.Number({ default: 9119 }),
 
   /** Hostname of the ordhook node we'll use to register predicates */
   ORDHOOK_NODE_RPC_HOST: Type.String({ default: '127.0.0.1' }),
